38b7ee6bde208177216b5fb58355dcd110cda455
fatima_97 <160069921+fatima317@users.noreply.github.com>
2025-01-01 23:14:06 +0500

    main

---
+++ task_2/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import About from "./components/About";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is HomePage</h1>
+      <Header/>
+      <About/>
+    </div>
+  );
+}
---
+++ task_4/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Card from './components/Card';
+import React from 'react';
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center min-h-screen bg-slate-300">
+      <div className="flex flex-wrap justify-center">
+    <Card name='Ali Akbar' rollno={3947} day='Monday' imageSrc="/profile_picture.jpeg"/><br />
+    <Card name='Iftikhar Ahmed' rollno={3950} day='Wednesday' imageSrc="/profile_picture.jpeg"/><br />
+    <Card name='Ahmed Raza' rollno={3960} day='Monday' imageSrc="/profile_picture.jpeg"/><br />
+    <Card name='Imran Khan' rollno={3972} day='Friday' imageSrc="/profile_picture.jpeg"/><br />
+    </div>
+    </div>
+  );
+}
